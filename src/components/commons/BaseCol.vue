39f0717d6a9644ddc22a9e3129e1af6eb8bfd7cb
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-06-16 19:28:25 +0200

    add ProductsSection

---
+++ src/components/commons/BaseCol.vue
@@ -0,0 +1,90 @@
+<template>
+    <div class="col">
+        <h5 class="col__title">{{col.title}}</h5>
+        <div class="col__products">
+            <div class="row" v-for="(product, i) in col.products" :key="i">
+                <div class="row__info">
+                    <span class="row__name">{{product.name}}</span>
+                    <div class="row__stars" v-show="product.isStar">
+                        <i v-for="i in 5" :key="i" class="fa-solid fa-star"></i>
+                    </div>
+                    <span class="row__promo-price" v-show="product.promoPrice">{{product.promoPrice}}</span>
+                    <span class="row__price" v-show="product.price">{{product.price}}</span>
+                    <small class="row__creator" v-show="product.creator">{{product.creator}}</small>
+                </div>
+                <div class="row__image">
+                    <img class="row__img" :src="require(`../../assets/img/${product.src}`)" :alt="product.name">
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'BaseCol',
+        props: {
+            col: Object
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import '../../assets/style/mixins.scss';
+
+    .col {
+        display: flex;
+        flex-direction: column;
+        color: var(--septenary-color-text);
+
+        &__title {
+            text-transform: uppercase;
+            color: var(--septenary-color-text);
+            font-weight: 500;
+            margin-bottom: 1.875rem; 
+        }
+
+        .row {
+            @include dflex(space-between);
+            align-items: flex-start;
+            border-bottom: 1px solid var(--primary-bg-color);
+            padding: .625rem 0;
+            
+            &__info {
+                font-size: .875rem;
+                line-height: 1.5625rem;
+
+                span {
+                    display: inline-block;
+                }
+            }
+
+            &__name {
+                margin-right: .3125rem;
+            }
+
+            &__stars {
+                color: var(--quaternary-color-text);
+            }
+
+            &__promo-price {
+                text-decoration: line-through;
+                margin-right: .3125rem;
+            }
+
+            &__price {
+                text-decoration: underline;
+            }
+
+            &__promo-price,
+            &__price {
+                font-weight: 600;
+                color: var(--secondary-color-text);
+            }
+
+            &__img {
+                height: 5rem;
+            }
+        }
+    }
+</style>
--- src/components/sections/ProductsSection.vue
+++ src/components/sections/ProductsSection.vue
@@ -1,17 +1,146 @@
 <template>
     <section class="products">
-
+        <div class="container">
+            <div class="products__box">
+                <BaseCol class="products__col" v-for="(col, i) in cols" :key="i" :col="col"/>
+            </div>
+        </div>
     </section>
 </template>
 
 <script>
+    import BaseCol from '../commons/BaseCol.vue'
+
     export default {
         name: 'ProductsSection',
+        components: {
+            BaseCol
+        },
+        data() {
+            return {
+                cols: [
+                    {
+                        title: 'Featured',
+                        products: [
+                            {
+                                name: 'Black Jacket',
+                                isStar: false,
+                                price: '$125',
+                                src: 'black_leather_jacket-120x156.jpg',
+                            },
+                            {
+                                name: 'Black Leather Jacket',
+                                isStar: true,
+                                promoPrice: '$235',
+                                price: '$200',
+                                src: 'black_elegant_leather_jacket-120x156.jpg',
+                            },
+                            {
+                                name: 'Hipster Black Top',
+                                isStar: false,
+                                price: '$57',
+                                src: 'hipster_black_top-120x156.jpg',
+                            },
+
+                        ]
+                    },
+                    {
+                        title: 'On sale',
+                        products: [
+                            {
+                                name: 'Black Leather Jacket',
+                                isStar: true,
+                                promoPrice: '$235',
+                                price: '$200',
+                                src: 'black_elegant_leather_jacket-120x156.jpg',
+                            },
+                            {
+                                name: 'Blue Leather Jacket',
+                                isStar: false,
+                                promoPrice: '$80',
+                                price: '$60',
+                                src: 'blue_leather_jacket-120x156.jpg',
+                            },
+                            {
+                                name: 'Modern Leather Boots',
+                                isStar: false,
+                                promoPrice: '$50',
+                                price: '$30',
+                                src: 'modern_leather_boots-120x156.jpg',
+                            },
+
+                        ]
+                    },
+                    {
+                        title: 'Top rated',
+                        products: [
+                            {
+                                name: 'Leather Gloves',
+                                isStar: true,
+                                price: '$45',
+                                src: 'leather_gloves-120x156.jpg',
+                            },
+                            {
+                                name: 'Black Leather Jacket',
+                                isStar: true,
+                                promoPrice: '$235',
+                                price: '$200',
+                                src: 'black_elegant_leather_jacket-120x156.jpg',
+                            },
+                            {
+                                name: 'Spring Printed Dress',
+                                isStar: true,
+                                price: '$47',
+                                src: 'spring_printed_dress-120x156.jpg',
+                            },
+
+                        ]
+                    },
+                    {
+                        title: 'Leatest review',
+                        products: [
+                            {
+                                name: 'Black Leather Jacket',
+                                isStar: true,
+                                creator: 'by admin',
+                                src: 'black_elegant_leather_jacket-120x156.jpg',
+                            },
+                            {
+                                name: 'Leather Gloves',
+                                isStar: true,
+                                creator: 'by Beardman',
+                                src: 'leather_gloves-120x156.jpg',
+                            },
+                            {
+                                name: 'Spring Printed Dress',
+                                isStar: true,
+                                creator: 'by admin',
+                                src: 'spring_printed_dress-120x156.jpg',
+                            },
+
+                        ]
+                    },
+
+
+                ]
+            }
+        }
     }
 </script>
 
 <style lang="scss" scoped>
+    @import '../../assets/style/mixins.scss';
+
     .products {
-        margin: 5rem 0;
+        margin: 2.5rem 0;
+
+        &__box {
+            @include dflex(space-between);
+        }
+
+        &__col {
+            width: calc(100% / 4 - 1.875rem);
+            margin: .9375rem;
+        }
     }
 </style>
