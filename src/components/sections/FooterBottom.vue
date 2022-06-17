16dd1d710442e0b96d796c28beb106b9cb30a0f8
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-06-17 10:38:45 +0200

    add FooterBottom

--- src/components/sections/FooterBottom.vue
+++ src/components/sections/FooterBottom.vue
@@ -1,6 +1,14 @@
 <template>
-    <div>
-
+    <div class="footer-bottom">
+        <div class="container">
+            <small class="footer-bottom__text">
+                <i class="fa-regular fa-copyright"></i> Copyright 2012 - 2020  |  
+                Avada Theme by Theme Fusion  |  
+                All Rights Reserved  |  
+                Powered by WordPress
+            </small>
+            <img class="footer-bottom__img" src="../../assets/img/payment_cards_footer.png" alt="payment cards">
+        </div>
     </div>
 </template>
 
@@ -11,5 +19,15 @@
 </script>
 
 <style lang="scss" scoped>
+    .footer-bottom {
+        background-color: var(--quaternary-bg-color);
+        padding: 1.875rem 0;
+        text-align: center;
 
+        &__text {
+            color: var(--quinary-color-text);
+            display: block;
+            margin-bottom: 1.25rem;
+        }
+    }
 </style>
